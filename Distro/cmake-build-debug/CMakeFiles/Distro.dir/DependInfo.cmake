867e5886a0bd8b9330014e8ce471c07b24ca6117
TylarZhu <zhuxingyuan123@gmail.com>
2018-10-08 16:26:25 -0700

    first commit

---
+++ Distro/cmake-build-debug/CMakeFiles/Distro.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xingyuanzhu/Desktop/CSCC85/project/p2/Distro/EV3_RobotControl/btcomm_test.c" "/home/xingyuanzhu/Desktop/CSCC85/project/p2/Distro/cmake-build-debug/CMakeFiles/Distro.dir/EV3_RobotControl/btcomm_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Distro/cmake-build-debug/CMakeFiles/Distro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Distro.dir/EV3_RobotControl/btcomm_test.c.o"
+  "Distro.pdb"
+  "Distro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Distro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
